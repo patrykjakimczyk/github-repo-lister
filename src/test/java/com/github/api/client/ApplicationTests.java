93951548df927d1aaff63a2cb69d0191c71d5c33
patrykjakimczyk <patr.jakimczyk@gmail.com>
2024-02-21 11:36:58 +0100

    Project initialization

---
+++ src/test/java/com/github/api/client/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.api.client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
